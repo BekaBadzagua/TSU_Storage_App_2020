10ec41aa8a827031307ac13c87febddc0b1569a1
BekaBadzagua <beka.badzagua@gmail.com>
2020-12-01 14:52:19 +0400

    Revert "UOW applyed to controllers"
    This reverts commit 73de66b00a03d6ead17bf19ced408918e05a6ce1.

--- StorageApp/Controllers/AuthController.cs
+++ StorageApp/Controllers/AuthController.cs
@@ -3,21 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Service.Interfaces;
 
 namespace StorageApp.Controllers
 {
     public class AuthController : Controller
     {
-        private readonly IUOW _uow;
-
-
-        public AuthController(IUOW uOW)
-        {
-            _uow = uOW;
-        }
-
-
         public IActionResult Index()
         {
             return View();
