73de66b00a03d6ead17bf19ced408918e05a6ce1
BekaBadzagua <beka.badzagua@gmail.com>
2020-12-01 14:18:41 +0400

    UOW applyed to controllers

--- StorageApp/Controllers/StoreController.cs
+++ StorageApp/Controllers/StoreController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Service.Interfaces;
 
 namespace StorageApp.Controllers
 {
     public class StoreController : Controller
     {
+        private readonly IUOW _uow;
+
+
+        public StoreController(IUOW uOW)
+        {
+            _uow = uOW;
+        }
+
         public IActionResult Index()
         {
             return View();
