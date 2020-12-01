103c703fa6f7363773068b3e3d55bf86b8411a49
BekaBadzagua <beka.badzagua@gmail.com>
2020-12-01 14:14:11 +0400

    Unit of Work Layer Tested - Success

--- StorageApp/Controllers/HomeController.cs
+++ StorageApp/Controllers/HomeController.cs
@@ -4,14 +4,37 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Service.Interfaces;
 using StorageApp.Models;
 
 namespace StorageApp.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly IUOW _uow;
+
+
+        public HomeController(IUOW uOW)
+        {
+            _uow = uOW;
+        } 
+
+
         public IActionResult Index()
         {
+            // TESTS
+
+            //var products = _uow.Product.FindAll();
+            //var product = _uow.Product.Get(1);
+
+            //var stores = _uow.Store.FindAll();
+            //var store = _uow.Store.Get(1);
+
+            //var storeProducts = _uow.StoreProduct.FindAll();
+            //var storeProduct = _uow.StoreProduct.Get(1);
+
+
             return View();
         }
 
