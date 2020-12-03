b2f6f845bebfddd2bec2ef07dd3dc477ea548423
tr0nn <47710546+tr0nn@users.noreply.github.com>
2020-12-03 13:20:08 +0400

    login registration update

--- StorageApp/Controllers/AuthController.cs
+++ StorageApp/Controllers/AuthController.cs
@@ -8,11 +8,11 @@ namespace StorageApp.Controllers
 {
     public class AuthController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
         }
-        public IActionResult LogIn()
+        public IActionResult Register()
         {
             return View();
         }
